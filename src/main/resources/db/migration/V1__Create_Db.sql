158d0734d8df24ae1b672a05d2fdfd4acfd83383
Laurent THIEBAULT <external.z05lthie@decathlon.com>
2017-10-05 01:14:27 +0200

    chore(CI): Configuration of Travis and Heroku
    Integration of creation and insertion script of the database + configuration flyway
    
    Commit to save the work before getting modifications from master

---
+++ src/main/resources/db/migration/V1__Create_Db.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Home (
+idhome varchar(50),
+homename varchar(100),
+CONSTRAINT pk_home_id PRIMARY KEY(idhome)
+);
+
+
+CREATE TABLE Person (
+personname varchar(50),
+password varchar(50),
+mail varchar(255),
+phonenumber varchar(10),
+address varchar(255),
+idhome varchar(50),
+isadmin smallint,
+CONSTRAINT pk_person_mail PRIMARY KEY(mail),
+CONSTRAINT fk_person_home FOREIGN KEY(idhome) REFERENCES home(idhome)
+);
+
+
+CREATE TABLE Home_Person(
+idhome varchar(50),
+mail varchar(255),
+CONSTRAINT pk_home_person PRIMARY KEY(mail,idhome),
+CONSTRAINT fk_home_person_id FOREIGN KEY(idhome) REFERENCES home(idhome),
+CONSTRAINT fk_home_person_mail FOREIGN KEY(mail) REFERENCES person(mail)
+);
+
+
+CREATE TABLE Product(
+barcode varchar(13),
+productname varchar(50),
+dlc date,
+qte int,
+image varchar(255),
+idhome varchar(50),
+CONSTRAINT pk_product_barcode PRIMARY KEY(barcode),
+CONSTRAINT fk_product_home FOREIGN KEY(idhome) REFERENCES home(idhome)
+);
