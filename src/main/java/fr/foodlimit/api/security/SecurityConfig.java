efb92fcce8afcfef035dfe8fcccd3fe0a84eebe6
Laurent THIEBAULT <external.z05lthie@decathlon.com>
2017-10-26 16:21:58 +0200

    feat(auth): setup the authentication

---
+++ src/main/java/fr/foodlimit/api/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package fr.foodlimit.api.security;
+
+import fr.foodlimit.api.security.jwt.JWTConfigurer;
+import fr.foodlimit.api.security.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+
+    public SecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/public").permitAll()
+                .antMatchers("/ping").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JWTConfigurer(this.tokenProvider));
+    }
+
+}
