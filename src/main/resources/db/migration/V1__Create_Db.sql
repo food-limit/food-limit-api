0100ef05f76cfbcccc1a9a780f19e99168f91e7c
Duquesne Florian <florian.duquesne1@gmail.com>
2017-11-08 11:34:03 +0100

    Add underscores in column's name

--- src/main/resources/db/migration/V1__Create_Db.sql
+++ src/main/resources/db/migration/V1__Create_Db.sql
@@ -1,39 +1,39 @@
 CREATE TABLE Home (
-idhome varchar(50),
-homename varchar(100),
-CONSTRAINT pk_home_id PRIMARY KEY(idhome)
+id_home varchar(50),
+home_name varchar(100),
+CONSTRAINT pk_home_id PRIMARY KEY(id_home)
 );
 
 
 CREATE TABLE Person (
-personname varchar(50),
+person_name varchar(50),
 password varchar(50),
 mail varchar(255),
-phonenumber varchar(10),
+phone_number varchar(10),
 address varchar(255),
-idhome varchar(50),
-isadmin smallint,
+id_home varchar(50),
+is_admin smallint,
 CONSTRAINT pk_person_mail PRIMARY KEY(mail),
-CONSTRAINT fk_person_home FOREIGN KEY(idhome) REFERENCES home(idhome)
+CONSTRAINT fk_person_home FOREIGN KEY(id_home) REFERENCES home(id_home)
 );
 
 
 CREATE TABLE Home_Person(
-idhome varchar(50),
+id_home varchar(50),
 mail varchar(255),
-CONSTRAINT pk_home_person PRIMARY KEY(mail,idhome),
-CONSTRAINT fk_home_person_id FOREIGN KEY(idhome) REFERENCES home(idhome),
+CONSTRAINT pk_home_person PRIMARY KEY(mail,id_home),
+CONSTRAINT fk_home_person_id FOREIGN KEY(id_home) REFERENCES home(id_home),
 CONSTRAINT fk_home_person_mail FOREIGN KEY(mail) REFERENCES person(mail)
 );
 
 
 CREATE TABLE Product(
 barcode varchar(13),
-productname varchar(50),
+product_name varchar(50),
 dlc date,
 qte int,
 image varchar(255),
-idhome varchar(50),
+id_home varchar(50),
 CONSTRAINT pk_product_barcode PRIMARY KEY(barcode),
-CONSTRAINT fk_product_home FOREIGN KEY(idhome) REFERENCES home(idhome)
+CONSTRAINT fk_product_home FOREIGN KEY(id_home) REFERENCES home(id_home)
 );
