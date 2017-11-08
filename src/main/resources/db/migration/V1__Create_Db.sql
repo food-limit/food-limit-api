2453b8c8027cfced4f8b79f3f115ed57ea3aedf4
Duquesne Florian <florian.duquesne1@gmail.com>
2017-11-08 14:02:52 +0100

    Edit column's name

--- src/main/resources/db/migration/V1__Create_Db.sql
+++ src/main/resources/db/migration/V1__Create_Db.sql
@@ -1,39 +1,39 @@
 CREATE TABLE Home (
-id_home varchar(50),
+home_id varchar(50),
 home_name varchar(100),
-CONSTRAINT pk_home_id PRIMARY KEY(id_home)
+CONSTRAINT pk_home_id PRIMARY KEY(home_id)
 );
 
 
 CREATE TABLE Person (
 person_name varchar(50),
-password varchar(50),
-mail varchar(255),
-phone_number varchar(10),
-address varchar(255),
-id_home varchar(50),
-is_admin smallint,
-CONSTRAINT pk_person_mail PRIMARY KEY(mail),
-CONSTRAINT fk_person_home FOREIGN KEY(id_home) REFERENCES home(id_home)
+person_password varchar(50),
+person_mail varchar(255),
+person_phone_number varchar(10),
+person_address varchar(255),
+person_home_id varchar(50),
+person_is_admin smallint,
+CONSTRAINT pk_person_mail PRIMARY KEY(person_mail),
+CONSTRAINT fk_person_home FOREIGN KEY(person_home_id) REFERENCES home(home_id)
 );
 
 
 CREATE TABLE Home_Person(
-id_home varchar(50),
-mail varchar(255),
-CONSTRAINT pk_home_person PRIMARY KEY(mail,id_home),
-CONSTRAINT fk_home_person_id FOREIGN KEY(id_home) REFERENCES home(id_home),
-CONSTRAINT fk_home_person_mail FOREIGN KEY(mail) REFERENCES person(mail)
+home_person_home_id varchar(50),
+home_person_mail varchar(255),
+CONSTRAINT pk_home_person PRIMARY KEY(home_person_mail, home_person_home_id),
+CONSTRAINT fk_home_person_id FOREIGN KEY(home_person_home_id) REFERENCES home(home_id),
+CONSTRAINT fk_home_person_mail FOREIGN KEY(home_person_mail) REFERENCES person(person_mail)
 );
 
 
 CREATE TABLE Product(
-barcode varchar(13),
+product_barcode varchar(13),
 product_name varchar(50),
-dlc date,
-qte int,
-image varchar(255),
-id_home varchar(50),
-CONSTRAINT pk_product_barcode PRIMARY KEY(barcode),
-CONSTRAINT fk_product_home FOREIGN KEY(id_home) REFERENCES home(id_home)
+product_dlc date,
+product_qte int,
+product_image varchar(255),
+product_home_id varchar(50),
+CONSTRAINT pk_product_barcode PRIMARY KEY(product_barcode),
+CONSTRAINT fk_product_home FOREIGN KEY(product_home_id) REFERENCES home(home_id)
 );
