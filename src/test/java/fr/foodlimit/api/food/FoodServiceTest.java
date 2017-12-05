5a1e25020e49cfad9207beeb3648589dacbad9de
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2017-12-05 17:37:57 +0100

    test(coverage): Food CRUD

--- src/test/java/fr/foodlimit/api/food/FoodServiceTest.java
+++ src/test/java/fr/foodlimit/api/food/FoodServiceTest.java
@@ -6,6 +6,7 @@ import fr.foodlimit.api.shared.models.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
@@ -32,12 +34,14 @@ public class FoodServiceTest {
 
   User user;
 
+  List<Food> foods;
+
   @Before
   public void init() {
-    List<Food> foods = new ArrayList<>();
+    this.foods = new ArrayList<>();
     Food yahourt = new Food();
     yahourt.setName("yahourt");
-    foods.add(yahourt);
+    this.foods.add(yahourt);
 
     user = new User();
     user.setUsername("test");
@@ -48,9 +52,48 @@ public class FoodServiceTest {
   }
 
   @Test
-  public void shouldCallPingRepositoryMethod() {
+  public void shouldGetFoods() {
     when(this.foodRepository.findByUser(Mockito.any())).thenReturn(user.getFoods());
 
     assertEquals(this.foodService.getFoods("test"), user.getFoods());
+  }
+
+  @Test
+  public void shouldGetFood() {
+    when(this.foodRepository.findById(1L)).thenReturn(Optional.of(this.foods.get(0)));
+
+    assertEquals(this.foodService.getFood(1L), user.getFoods().get(0));
+  }
+
+  @Test
+  public void shouldDeleteFood() {
+    ArgumentCaptor<Long> longCaptor = ArgumentCaptor.forClass(Long.class);
+
+    this.foodService.deleteFood(1L);
+
+    Mockito.verify(foodRepository).deleteById(longCaptor.capture());
+
+    assertEquals(new Long(1L), longCaptor.getValue());
+  }
+
+  @Test
+  public void shouldCreateFood() {
+    Food food = new Food();
+    food.setId(2L);
+
+    when(this.foodRepository.save(Mockito.any())).thenReturn(food);
+
+    assertEquals(this.foodService.createFood(food, "test").getId(), null);
+    assertEquals(this.foodService.createFood(food, "test").getName(), food.getName());
+  }
+
+  @Test
+  public void shouldUpdateFood() {
+    Food food = new Food();
+    food.setId(3L);
+
+    when(this.foodRepository.save(Mockito.any())).thenReturn(food);
+
+    assertEquals(this.foodService.updateFood(food, "test").getName(), food.getName());
   }
 }
